1a8b77a82b7633d03f3aaada1fa560e019d03306
SHIROKAMIQQ <migzguiang@gmail.com>
2024-10-13 06:37:11 +0800

    feat(LAB6f.c): implement mra_min_range()

--- LAB06/LAB6f.c
+++ LAB06/LAB6f.c
@@ -18,14 +18,24 @@ mra_node* recursive_init(int64_t* data, size_t i, size_t j) {
     }
     return root;
 }
-
 min_ready_array* mra_init(size_t n, int64_t* data) {
     min_ready_array* mra = (min_ready_array*)malloc(sizeof(min_ready_array));
     if (n == 0) {mra->root = NULL;}
     else {mra->root = recursive_init(data, 0, n);}
     return mra;
 }
-int64_t mra_min_range(min_ready_array *a, idx_t i, idx_t j);
+
+int64_t recursive_min(mra_node* root, idx_t i, idx_t j) {
+    if (root->ri <= i || root->li >= j) {return INT64_MAX;}
+    if (root->li >= i && root->ri <= j) {return root->val;}
+    if (root->ri - root->li == 1) {return root->val;}
+    int64_t left_min = recursive_min(root->left, i, j);
+    int64_t right_min = recursive_min(root->right, i, j);
+    return (left_min <= right_min) ? left_min : right_min;
+}
+int64_t mra_min_range(min_ready_array *a, idx_t i, idx_t j) {
+    return recursive_min(a->root, i, j);
+}
 
 int32_t mra_size(min_ready_array *a) {
     if (a->root == NULL){return 0;}
