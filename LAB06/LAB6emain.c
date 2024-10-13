757e39ef19444e3fada796eef13c4fd2ab58c1da
SHIROKAMIQQ <migzguiang@gmail.com>
2024-10-13 19:44:02 +0800

    test(LAB6e): implement dynamic array; add test cases for ph_push() and ph_size()

---
+++ LAB06/LAB6emain.c
@@ -0,0 +1,77 @@
+#include "podium_heap.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef struct dynamic_array{
+    int64_t* arr;
+    idx_t size;
+} dynamic_array;
+dynamic_array *da_init(void){
+    dynamic_array* da = (dynamic_array*)malloc(sizeof(dynamic_array));
+    int64_t* a = (int64_t*)malloc(100*sizeof(int64_t));
+    da->arr = a;
+    da->size = 0;
+    return da;
+}
+void da_set(dynamic_array *a, idx_t i, int64_t val){
+    (a->arr)[i] = val;
+}
+int64_t da_get(dynamic_array *a, idx_t i){
+    return (a->arr)[i];
+}
+void da_push(dynamic_array *a, int64_t val){
+    (a->arr)[(a->size)++] = val;
+}
+int64_t da_pop(dynamic_array *a){
+    return (a->arr)[--(a->size)];
+}
+int32_t da_size(dynamic_array *a){
+    return (a->size);
+}
+bool da_empty(dynamic_array *a){
+    return (a->size == 0) ? true : false;
+} 
+
+podium_heap* ph_init() {
+    podium_heap *h = (podium_heap*)malloc(sizeof(podium_heap));
+    h->vals = da_init();
+    h->size = 0;
+    assert(ph_empty(h));
+    return h;
+}
+
+int64_t ph_peek_max(podium_heap* h) {
+    assert(!ph_empty(h));
+    return da_get(h->vals, 0);
+}
+
+bool ph_empty(podium_heap* h) {
+    return ph_size(h) == 0;
+}
+
+void print_list(int64_t* arr, idx_t length) {
+    if (length == 0) {printf("NULL");}
+    else {
+        for (idx_t i = 0; i < length; i++) {
+            printf("%ld ", arr[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    podium_heap* ph1 = ph_init();
+    printf("EMPTY: %d\n", ph_empty(ph1));
+    ph_push(ph1, 50);
+    printf("PUSHED 50: "); print_list(ph1->vals->arr, ph_size(ph1));
+    printf("SIZE: %d\n", ph_size(ph1));
+    ph_push(ph1, 60);
+    printf("PUSHED 60: "); print_list(ph1->vals->arr, ph_size(ph1));
+    printf("SIZE: %d\n", ph_size(ph1));
+    ph_push(ph1, 1);
+    printf("PUSHED 1: "); print_list(ph1->vals->arr, ph_size(ph1));
+    printf("SIZE: %d\n", ph_size(ph1));
+}
