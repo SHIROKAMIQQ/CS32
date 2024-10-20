db4598ee6360bd11ca751350ebd52020f0e23aa9
SHIROKAMIQQ <migzguiang@gmail.com>
2024-10-20 23:59:04 +0800

    perf(LAB7d.c): implement a solution to reset the grid by following the path traversed

--- LAB07/LAB7d.c
+++ LAB07/LAB7d.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool traversable(grid* g, int i, int j, int64_t x, bool** onion);
-int traverse(grid* g, int i, int j, int64_t x, bool** onion);
+#define E12 1000000000000
 
-bool traversable(grid* g, int i, int j, int64_t x, bool** onion) {
+bool traversable(grid* g, int i, int j, int64_t x);
+int traverse(grid* g, int i, int j, int64_t x);
+void reset(grid* g, int i, int j);
+
+bool traversable(grid* g, int i, int j, int64_t x) {
     if (
         ((0 <= i && i < (g->r)) && (0 <= j && j < (g->c))) &&
-        ((*onion)[(i*(g->c))+j] == true) &&
         (x >= ((g->h)[i][j])) 
     ){
         return true;
@@ -18,22 +20,33 @@ bool traversable(grid* g, int i, int j, int64_t x, bool** onion) {
     }
 }
 
-int traverse(grid* g, int i, int j, int64_t x, bool** onion){
+int traverse(grid* g, int i, int j, int64_t x){
     int ret = 0;
-    if (traversable(g, i, j, x, onion) == true){
-        (*onion)[(i*(g->c))+j] = false;
+    if (traversable(g, i, j, x) == true){
         ret++;
-        ret += traverse(g, i+1, j, x, onion);
-        ret += traverse(g, i-1, j, x, onion);
-        ret += traverse(g, i, j+1, x, onion);
-        ret += traverse(g, i, j-1, x, onion);
+        (g->h)[i][j] += E12;
+        ret += traverse(g, i+1, j, x);
+        ret += traverse(g, i-1, j, x);
+        ret += traverse(g, i, j+1, x);
+        ret += traverse(g, i, j-1, x);
     }
     return ret;
 }
 
+void reset(grid* g, int i, int j) {
+    if (
+        ((0 <= i && i < (g->r)) && (0 <= j && j < (g->c))) &&
+        (((g->h)[i][j]) >= E12) 
+    ) {
+        (g->h)[i][j] -= E12;
+        reset(g, i+1, j);
+        reset(g, i-1, j);
+        reset(g, i, j+1);
+        reset(g, i, j-1);
+    } 
+}
+
 void onions(grid *g, int q, query *qs){
-    bool* onion = (bool*)malloc((g->r)*(g->c)*sizeof(bool));
-    for (int i = 0; i < (g->r)*(g->c); i++) {onion[i] = true;}
     for (int i = 0; i < q; i++) {
         query que = qs[i];
         int i1 = que.i1; 
@@ -42,10 +55,10 @@ void onions(grid *g, int q, query *qs){
         int j2 = que.j2;
         int64_t x = que.x;
         int ans = 0;
-        ans += traverse(g, i1, j1, x, &onion);
-        ans += traverse(g, i2, j2, x, &onion);
+        ans += traverse(g, i1, j1, x);
+        ans += traverse(g, i2, j2, x);
         (qs[i]).ans = ans;
-        for (int i = 0; i < (g->r)*(g->c); i++){onion[i]=true;}
+        reset(g, i1, j1);
+        reset(g, i2, j2);
     }
-    free(onion);
 }
