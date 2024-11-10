f2a9e37c2c9c3e403402cc93730efeb268493ab3
SHIROKAMIQQ <migzguiang@gmail.com>
2024-11-11 06:19:34 +0800

    test(LAB8e): add test case for guess_friends()

--- LAB08/LAB8emain.c
+++ LAB08/LAB8emain.c
@@ -6,21 +6,26 @@ typedef struct tuple {
     int idx;
     int fs;
 } tuple;
-tuple* make_tuples(int n, int* f);
-void merge_sort(tuple* arr, int left, int right);
 
-void print_tuples(int n, tuple* t) {
-    for (int i = 0; i < n; i++) {
-        printf("(%d, %d), ", t[i].idx, t[i].fs);
+void print_friends(friends* f) {
+    if (f->count == -1) {
+        printf("IMPOSSIBLE\n");
+        return;
+    }
+    for (int i = 0; i < f->count; i++){
+        printf("(%d, %d), ", (f->pairs)[i].person1, (f->pairs)[i].person2);
     }
     printf("\n");
 }
 
 int main() {
-    int n = 10;
-    int f[10] = {3,2,5,6,2,5,1,7,4,2};
-    tuple* t = make_tuples(n, f);
-    print_tuples(n, t);
-    merge_sort(t, 0, n-1);
-    print_tuples(n, t);
+    int n1 = 6;
+    int f1[6] = {2,2,0,2,1,3};
+    friends* friends1 = guess_friends(n1, f1);
+    print_friends(friends1);
+
+    int n2 = 3;
+    int f2[3] = {0,2,2};
+    friends* friends2 = guess_friends(n2, f2);
+    print_friends(friends2);
 }
